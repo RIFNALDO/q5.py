2d1976c30d4c46da4b71d178d0215b4334496425
RIFNALDO <rumana3712@gmail.com>
2026-07-04 20:04:34 -0400

    Add files via upload

---
+++ q5.py
@@ -0,0 +1,12 @@
+a = (input("Do you have a Driving licencse Yes/No   :"))
+b = int(input("How old are you   :"))
+
+if a == "Yes" and b >= 18:
+    print("You are allowed to drive")
+
+else:
+    print("You are not allowed to drive")
+
+
+
+
